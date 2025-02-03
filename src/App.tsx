9a32a17b4cfa692e6caa823d8f5fd89fd4431b85
Ritvik718 <ritvikranjan22@gmail.com>
2025-02-03 18:39:02 +0530

    added header, footer, signin , signup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-// import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import PublicLayout from "@/layouts/public-layout";
+import AuthenticationLayout from "@/layouts/auth-layout";
+
+import HomePage from "@/routes/home";
+import SignUp from "./routes/sign-up";
+import SignIn from "./routes/sign-in";
 
 const App = () => {
-  return <div className="text-blue-700">App</div>;
+  return (
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+
+        {/* protected routes */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
