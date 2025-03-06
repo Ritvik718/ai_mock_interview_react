6c944ab8f5876f9cf04f92738d38e62f7e64075b
Ritvik718 <ritvikranjan22@gmail.com>
2025-03-06 21:49:11 +0530

    Changes: Project Completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PublicLayout from "@/layouts/public-layout";
 import AuthenticationLayout from "@/layouts/auth-layout";
-import MainLayout from "./layouts/main-layout";
+import ProtectRoutes from "@/layouts/protected-routes";
 
+import MainLayout from "@/layouts/main-layout";
 import HomePage from "@/routes/home";
 import SignUpPage from "./routes/sign-up";
 import SignInPage from "./routes/sign-in";
-import ProtectRoutes from "./layouts/protected-routes";
 import { Generate } from "./components/generate";
 import { Dashboard } from "./routes/dashboard";
 import { CreateEditPage } from "./routes/create-edit-page";
+import { MockLoadPage } from "./routes/mock-load-page";
+import { MockInterviewPage } from "./routes/mock-interview-page";
+import { Feedback } from "./routes/feedback";
 
 const App = () => {
   return (
@@ -35,10 +38,16 @@ const App = () => {
             </ProtectRoutes>
           }
         >
-          {/* add all protected routes */}
+          {/* add all the protect routes */}
           <Route element={<Generate />} path="/generate">
             <Route index element={<Dashboard />} />
             <Route path=":interviewId" element={<CreateEditPage />} />
+            <Route path="interview/:interviewId" element={<MockLoadPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
           </Route>
         </Route>
       </Routes>
